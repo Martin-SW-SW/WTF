4d0346e4468f37af29b4582e8a98b93d643da71e
Martin-SW-SW <62882502+Martin-SW-SW@users.noreply.github.com>
2020-03-30 13:33:57 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WTF
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+                 
+        System.Console.WriteLine(MysticNumbers.FourNumbers(3,290,50,10));
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ MysticNumbers.cs
@@ -0,0 +1,37 @@
+﻿namespace WTF
+{
+    public class MysticNumbers
+    {
+        #region Methods
+        public static int ThreeNumbers(int a, int b, int c)
+        {
+            int result;
+            result = DoubleNumbers(a,b);
+            return result = DoubleNumbers(result, c);
+        } 
+        public static int FourNumbers(int a, int b, int c, int d)
+        {
+            int result;
+            result = ThreeNumbers(a,b,c);
+            return result = DoubleNumbers(result, d);
+        } 
+        public static int DoubleNumbers(int a, int b)
+        {
+                    int result;
+
+            if (b > a)
+            {
+                result = b;
+            }
+            else
+            {
+                result = a;
+            }
+
+            return result;
+
+        }
+    }
+        #endregion
+    
+}
